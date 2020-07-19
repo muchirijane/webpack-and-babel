3b7785d32ac9d82a26925d5a9cafd726883994eb
jane muthoni <muchirijane@outlook.com>
2020-07-20 02:00:29 +0300

    wp and bl

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,18 @@ module.exports = {
     devServer : {
         contentBase: path.resolve(__dirname, 'dist'),
         publicPath: '/assets/'
+    },
+    module : {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use : {
+                loader : 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
     }
 
 }
